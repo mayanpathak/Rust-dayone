1ecd569409d9ac166bbd33dd09187b8fc3895c35
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-14 06:24:51 +0530

    added lifetime

--- src/main.rs
+++ src/main.rs
@@ -71,7 +71,15 @@
 
 // }
 
+fn longest <'a>(x:&'a str, y: &'a str) -> &'a str{
+    if x.len() >= y.len() {x} else{y}
+}
+
 fn main (){
 
-    
+    let s1 = String::from("abcd");
+    let s2 = "xyz";
+
+    let result = longest (&s1, s2);
+    println!("The longest string is {}", result);
 }
